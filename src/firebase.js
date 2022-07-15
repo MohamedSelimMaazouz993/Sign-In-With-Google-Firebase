46139c35981ae3b44030baa486d9140771ed2cb2
MohamedSelimMaazouz993 <mohamedselim.maazouz@gmail.com>
2022-07-15 16:35:26 +0100

    1st commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR FIREBASE API KEY",
+  authDomain: "YOUR FIREBASE AUTHDOMAIN",
+  projectId: "YOUR FIREBASE PROJECTID",
+  storageBucket: "YOUR FIREBASE STORAGE BUCKET",
+  messagingSenderId: "YOUR FIREBASE MESSAGESENDER ID",
+  appId: "YOUR FIREBASE APPID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
